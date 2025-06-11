b2a52c792f310e97a05a8d5b447e1ea99fed6446
Naveen kumar <Naveen.Cenimeni@partner.bmwgroup.com>
2025-06-11 11:31:25 +0530

    added the rate limiter

--- serviceA/serviceA/src/main/java/com/naveen/serviceA/controller/ServiceAController.java
+++ serviceA/serviceA/src/main/java/com/naveen/serviceA/controller/ServiceAController.java
@@ -1,5 +1,6 @@
 package com.naveen.serviceA.controller;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,15 +18,15 @@ public class ServiceAController {
     private static final String BASE_URL = "http://localhost:8081/";
 
     private  static final String SERVICE_NAME = "ServiceA";
-
     @GetMapping
-    @Retry(name = SERVICE_NAME, fallbackMethod = "serviceFallBack")
+    @RateLimiter(name = SERVICE_NAME, fallbackMethod = "serviceFallBack")
     public String getMessage() {
         return restTemplate.getForObject(BASE_URL + "b", String.class);
     }
 
     public String serviceFallBack(Throwable throwable) {
-        return "This is a fallback response from " + SERVICE_NAME + ". Reason: " + throwable.getMessage();
+        return "Rate limit exceeded. Please try again later. Reason: " + throwable.getMessage();
     }
+
 
 }
