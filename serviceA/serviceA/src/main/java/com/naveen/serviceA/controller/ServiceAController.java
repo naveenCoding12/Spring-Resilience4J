ceeb9a09fe2b44519584c4bcc4cce8922058d9fa
Naveen kumar <Naveen.Cenimeni@partner.bmwgroup.com>
2025-06-11 11:11:03 +0530

    added the retry

--- serviceA/serviceA/src/main/java/com/naveen/serviceA/controller/ServiceAController.java
+++ serviceA/serviceA/src/main/java/com/naveen/serviceA/controller/ServiceAController.java
@@ -1,6 +1,6 @@
 package com.naveen.serviceA.controller;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +19,7 @@ public class ServiceAController {
     private  static final String SERVICE_NAME = "ServiceA";
 
     @GetMapping
-    @CircuitBreaker(name = SERVICE_NAME, fallbackMethod = "serviceFallBack")
+    @Retry(name = SERVICE_NAME, fallbackMethod = "serviceFallBack")
     public String getMessage() {
         return restTemplate.getForObject(BASE_URL + "b", String.class);
     }
